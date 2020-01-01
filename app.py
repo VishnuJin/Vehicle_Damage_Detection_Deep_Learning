2d9257665055a85cd42f48d58592d646b373abd9
vishnu <artharpain143@gmail.com>
2020-01-02 04:03:21 +0530

    ui created

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def index():
 
 @app.route('/<path:path>')
 def model(path):
-    print(path)
     return send_from_directory('Model',path)
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,186 @@
+// File Upload
+// 
+function ekUpload(){
+    function Init() {
+  
+      console.log("Upload Initialised");
+  
+      var fileSelect    = document.getElementById('file-upload'),
+          fileDrag      = document.getElementById('file-drag'),
+          submitButton  = document.getElementById('submit-button');
+  
+      fileSelect.addEventListener('change', fileSelectHandler, false);
+  
+      // Is XHR2 available?
+      var xhr = new XMLHttpRequest();
+      if (xhr.upload) {
+        // File Drop
+        fileDrag.addEventListener('dragover', fileDragHover, false);
+        fileDrag.addEventListener('dragleave', fileDragHover, false);
+        fileDrag.addEventListener('drop', fileSelectHandler, false);
+      }
+    }
+  
+    function fileDragHover(e) {
+      var fileDrag = document.getElementById('file-drag');
+  
+      e.stopPropagation();
+      e.preventDefault();
+  
+      fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
+    }
+  
+    function fileSelectHandler(e) {
+      // Fetch FileList object
+      var files = e.target.files || e.dataTransfer.files;
+  
+      // Cancel event and hover styling
+      fileDragHover(e);
+  
+      // Process all File objects
+      for (var i = 0, f; f = files[i]; i++) {
+        parseFile(f);
+        //uploadFile(f);
+      }
+    }
+  
+    // Output
+    function output(msg) {
+      // Response
+      var m = document.getElementById('messages');
+      m.innerHTML = msg;
+    }
+  
+    function parseFile(file) {
+    console.log(file)
+      console.log(file.name);
+      output(
+        '<strong>' + encodeURI(file.name) + '</strong>'
+      );
+      
+      // var fileType = file.type;
+      // console.log(fileType);
+      var imageName = file.name;
+  
+      var isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName);
+      if (isGood) {
+        document.getElementById('start').classList.add("hidden");
+        document.getElementById('response').classList.remove("hidden");
+        document.getElementById('notimage').classList.add("hidden");
+        // Thumbnail Preview
+        document.getElementById('file-image').classList.remove("hidden");
+        document.getElementById('file-image').src = URL.createObjectURL(file);
+        //console.log(URL.createObjectURL(file))
+        //document.getElementById("uploaded_image").src = URL.createObjectURL(file);
+       // setTimeout(()=>{
+         predict(document.getElementById('file-image'))
+       // },5000)
+        
+      }
+      else {
+        document.getElementById('file-image').classList.add("hidden");
+        document.getElementById('notimage').classList.remove("hidden");
+        document.getElementById('start').classList.remove("hidden");
+        document.getElementById('response').classList.add("hidden");
+        document.getElementById("file-upload-form").reset();
+      }
+    }
+  
+    function setProgressMaxValue(e) {
+      var pBar = document.getElementById('file-progress');
+  
+      if (e.lengthComputable) {
+        pBar.max = e.total;
+      }
+    }
+  
+    function updateFileProgress(e) {
+      var pBar = document.getElementById('file-progress');
+  
+      if (e.lengthComputable) {
+        pBar.value = e.loaded;
+      }
+    }
+  
+    function uploadFile(file) {
+  
+      var xhr = new XMLHttpRequest(),
+        fileInput = document.getElementById('class-roster-file'),
+        pBar = document.getElementById('file-progress'),
+        fileSizeLimit = 1024; // In MB
+      if (xhr.upload) {
+        // Check if file is less than x MB
+        if (file.size <= fileSizeLimit * 1024 * 1024) {
+          // Progress bar
+          pBar.style.display = 'inline';
+          xhr.upload.addEventListener('loadstart', setProgressMaxValue, false);
+          xhr.upload.addEventListener('progress', updateFileProgress, false);
+  
+          // File received / failed
+          xhr.onreadystatechange = function(e) {
+            if (xhr.readyState == 4) {
+              // Everything is good!
+  
+              // progress.className = (xhr.status == 200 ? "success" : "failure");
+              // document.location.reload(true);
+            }
+          };
+  
+          // Start upload
+          xhr.open('POST', document.getElementById('file-upload-form').action, true);
+          xhr.setRequestHeader('X-File-Name', file.name);
+          xhr.setRequestHeader('X-File-Size', file.size);
+          xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+          xhr.send(file);
+        } else {
+          output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
+        }
+      }
+    }
+  
+    // Check for the various File API support.
+    if (window.File && window.FileList && window.FileReader) {
+      Init();
+    } else {
+      document.getElementById('file-drag').style.display = 'none';
+    }
+  }
+  ekUpload();
+
+  const isInViewport = function (elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+window.addEventListener('scroll', () => {
+    const submission = document.getElementById('image_Submission');
+    if (isInViewport(submission)) {
+        document.body.style.backgroundColor="#fff";
+        document.getElementById("howToflex").style.backgroundColor = "#212121"
+    }else{
+        document.body.style.backgroundColor="#212121";
+    }
+})
+
+async function predict(image) {
+  //console.log(image)
+    const model = await tf.automl.loadImageClassification('model.json');
+    const predictions = await model.classify(image);
+    console.log(predictions);
+    // Show the resulting object on the page.
+    const results = predictions.sort((a,b)=>{
+      return b.prob-a.prob
+    })
+    topTwo = results.slice(0,2)
+    res = document.createElement('h2')
+    res.textContent = "Results:"
+    document.getElementById('file-drag').append(res)
+    const pre = document.createElement('p');
+    pre.textContent = `${topTwo[0].label},${topTwo[1].label}`//JSON.stringify(predictions, null, 2);
+    //document.body.append(pre);
+    document.getElementById('file-drag').append(pre)
+  }
